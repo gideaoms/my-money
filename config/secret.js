346dceecc1b651d9a178536f453402e049110fe0
Gideão Silva <gideaoms@gmail.com>
2018-07-02 15:39:57 -0300

    gitignore secret

---
+++ config/secret.js
@@ -0,0 +1,6 @@
+module.exports = {
+  DATABASE_PASSWORD: "123456",
+  FOREST_ENV_SECRET:
+    "0e3c9288e3ec4cf63a12aa4f6e2e0b526d95ac66dff49bb47233c6c949624ab6",
+  FOREST_AUTH_SECRET: "zashgBp1rhaAvpfjacyJj2it186zGqfo"
+};
